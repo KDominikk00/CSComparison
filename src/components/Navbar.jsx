699854e842a4a36d023f408d77dc61a3d1f2fef5
Dominik Kowalski <dominikkowalski331@gmail.com>
2024-11-01 19:07:13 +0100

    Create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-center'>
+      <img className='w-50 h-32' src="/CSComparison.png" alt="CSComparison" />
+    </nav>
+  );
+};
+
+export default Navbar;
