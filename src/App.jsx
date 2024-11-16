16e723adb212698ee66319881d61fddc8041c63f
Dominik Kowalski <dominikkowalski331@gmail.com>
2024-11-16 15:57:40 +0100

    Rework component load strategy for smoother performance

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Navbar from './components/Navbar'; 
 import { PlayerProvider } from './context/PlayerContext';
 import Main from './components/Main';
-import ExtraStats from './components/ExtraStats';
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <PlayerProvider>
-        <Main/>
-        <ExtraStats />
+        <Main />
       </PlayerProvider>
     </div>
   );
